479a85345d47c4971783552787c9b798e0efd856
Sufiyan Mistry <sufiyanmistry02@gmail.com>
2025-10-10 10:03:52 +0200

    Attributen privé gemaakt en databaseklasse verbeterd (feedback DP9 verwerkt) in db_connect.py

--- Startcode treintje/db_connect.py
+++ Startcode treintje/db_connect.py
@@ -1,25 +1,49 @@
 import mysql.connector
 
-try:
-    conn = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Mistry123!",
-        database="LSM_treintje"
-    )
+class DatabaseConnection:
+    def __init__(self): 
+        self._host = host
+        self._user = user
+        self._password = password
+        self._database = database
+        self._conn = None
+        self._cursor = None
 
-    cursor = conn.cursor()
+    def open_verbinding(self):
+        try:
+            self._conn = mysql.connector.connect(
+                host=self._host,
+                user=self._user,
+                password=self._password,
+                database=self._database
+            )
+            self._cursor = self._conn.cursor()
+            print("Verbinding met database succesvol.")
 
-    cursor.execute("SELECT locatie_id, naam, wachttijd FROM Locatie")
+        except mysql.connector.Error as err:
+            print(f"Fout bij verbinden met database: {err}")
 
-    print("Beschikbare locaties in de database:\n")
-    for (locatie_id, naam, wachttijd) in cursor.fetchall():
-        print(f"{locatie_id}: {naam} (wachttijd: {wachttijd} minuten)")
+    def toon_locaties(self):
+        try:  
+            self._cursor.execute("SELECT locatie_id, naam, wachttijd FROM Locatie")
+            print("Beschikbare locaties in de database:\n")
+            for (locatie_id, naam, wachttijd) in self._cursor.fetchall():
+                print(f"{locatie_id}: {naam} (wachttijd: {wachttijd} minuten)")
 
-except mysql.connector.Error as err:
-    print(f"Fout bij verbinden met database: {err}")
+        except mysql.connector.Error as err:
+            print(f"Fout bij ophalen van locaties: {err}")
 
-finally:
-    if conn.is_connected():
-        cursor.close()
-        conn.close()
+    def voeg_reservering_toe(self,attractie, tijdslot):
+        try:
+            query = "INSERT INTO Reservering (attractie, tijdslot) VALUES (%s, %s)"
+            self._cursor.execute(query, (attractie, tijdslot))
+            self._conn.commit()
+            print(f"Reservering succesvol toegevoegd: {attractie} om {tijdslot}.")
+        except mysql.connector.Error as err:
+            print(f"Fout bij toevoegen van reservering: {err}")
+
+    def sluit_verbinding(self):
+        if self._conn and self._conn.is_connected():
+             self._cursor.close()
+             self._conn.close()
+             print("Verbinding met database gesloten.")
